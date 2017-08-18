79a5df2e5017b811cc6910150bb99ccc437f7d3e
Stay <stay@StaydeMac-mini.local>
2017-08-18 14:17:56 +0800

    add

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 var server = require('./server.js');
+var route = require('./router');
+var requireHandlers = require('./requestHandlers');
+
+var handle = {};
+handle['/'] = requireHandlers.start;
+handle['/start'] = requireHandlers.start;
 server.start();
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = 3000;
 function start(){
 	function onRequest(req, res){
 		res.writeHead(200, 'Content-type: text/plain');
-		res.write('111111');
+		res.write('this is node server');
 		res.end();
 	}
 	http.createServer(onRequest).listen(port, function(){
