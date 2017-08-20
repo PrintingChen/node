23aa2a8005117f9a972ef9fb4ce2cbfdb07beb08
printing chen <2663251638@qq.com>
2017-08-20 22:29:02 +0800

    modify book_crawler.js

--- crawler/book_crawler.js
+++ crawler/book_crawler.js
@@ -3,9 +3,18 @@ var request = require('request');
 var fs = require('fs');
 var cheerio = require('cheerio');  //解析爬取的html
 var iconv = require('iconv-lite'); //处理爬取网页乱码问题
+var mongoose = require('../mongodb_demo/db.js');
 
+var Schema = mongoose.Schema;
+var schema = new Schema({
+	chapter: {type: String},
+	content: {type: String},
+	uploadDate: {type: Date},
+	isDelete: {type: Boolean}
+});
+var bookModel = mongoose.model('content', schema);
 
-var url = 'http://wodeshucheng.com/book_14790/3779830.html';
+var url = 'http://wodeshucheng.com/book_14790/4504708.html';
 function myRequest(url, cb){
 	var options = {
 		url: url,
@@ -20,19 +29,33 @@ function getContent(url){
 			console.log(response.statusCode)
 			var html = iconv.decode(body, 'gbk');
 			var $ = cheerio.load(html);
+			var chapterTitle = $('.h1title h1').text().split(' ')[1];
 			var $prevUrl =  $('#pager_prev').attr('href');
 			var $nextUrl =  $('#pager_next').attr('href');
 			$('#htmlContent').find('.chapter_Turnpage, center').remove();
-			var content = $('#htmlContent').text(); //当前页内容
-			fs.appendFile('../public/book.html', content, function(err){
+			var currentContent = $('#htmlContent').text(); //当前页内容
+			var chapterContent = new bookModel({
+				chapter: chapterTitle,
+				content: currentContent,
+				uploadDate: new Date(),
+				isDelete: 0
+			});
+			chapterContent.save(function(err, res){
 				if (err) {
-					console.log('appendFile:',err)
+					console.log('save error:', err)
 				}else{
-					console.log('appendFile success')
+					console.log('save success:', res)
 				}
 			});
+			// fs.appendFile('../public/book.html', content, function(err){
+			// 	if (err) {
+			// 		console.log('appendFile:',err)
+			// 	}else{
+			// 		console.log('appendFile success')
+			// 	}
+			// });
 			if ($nextUrl) {
-				getContent($prevUrl);
+				getContent($nextUrl);
 			}
 		}else{
 			console.log('request error:', error)
